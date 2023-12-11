3fb121653a16219f41d12ec6087db280b850ab43
luizfellipes <luiz.13.silva@gmail.com>
2023-12-11 14:04:37 -0400

    Adicionado uma tratativa de recebimento de objeto nulo

--- src/main/java/com/example/toolschallanger/models/dtos/TransacaoRecordDto.java
+++ src/main/java/com/example/toolschallanger/models/dtos/TransacaoRecordDto.java
@@ -4,8 +4,15 @@ import com.example.toolschallanger.models.entities.TransacaoModel;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 
-
 public record TransacaoRecordDto(@NotNull
                                  @JsonProperty
                                  TransacaoModel transacaoModel) {
+    @Override
+    public TransacaoModel transacaoModel() {
+        if (transacaoModel.getDescricaoModel() != null || transacaoModel.getFormaPagamentoModel() != null) {
+            return transacaoModel;
+        } else {
+            throw new RuntimeException("Transacao Vazia !");
+        }
+    }
 }
