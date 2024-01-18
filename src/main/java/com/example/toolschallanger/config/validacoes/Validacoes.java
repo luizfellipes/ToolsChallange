03654174bc3dc85caf9a91e7747da3ef6862573c
luizfellipes <luiz.13.silva@gmail.com>
2024-01-18 15:12:22 -0400

    removido exececoes e colocado em validacoes o codigo excedente

--- src/main/java/com/example/toolschallanger/config/validacoes/Validacoes.java
+++ src/main/java/com/example/toolschallanger/config/validacoes/Validacoes.java
@@ -1,4 +1,4 @@
-package com.example.toolschallanger.Config.Validacoes;
+package com.example.toolschallanger.config.validacoes;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -6,12 +6,13 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
-public class ValidaCampos {
+public class Validacoes {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -20,7 +21,13 @@ public class ValidaCampos {
         ex.getBindingResult().getFieldErrors().forEach(erro -> {
             camposVazios.put(erro.getField(), erro.getDefaultMessage());
         });
-        return ResponseEntity.badRequest().body(camposVazios);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(camposVazios);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Object> runtimeException(RuntimeException exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro encontrado: " + exception.getLocalizedMessage());
     }
 
 }
