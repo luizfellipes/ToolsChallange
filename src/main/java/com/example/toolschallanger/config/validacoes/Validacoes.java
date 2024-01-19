e03338b0f4220a0fcd94a45bf36c48d5e51637ef
luizfellipes <luiz.13.silva@gmail.com>
2024-01-19 13:11:18 -0400

    adicionado validacao de solicita nao permitida

--- src/main/java/com/example/toolschallanger/config/validacoes/Validacoes.java
+++ src/main/java/com/example/toolschallanger/config/validacoes/Validacoes.java
@@ -1,7 +1,10 @@
 package com.example.toolschallanger.config.validacoes;
 
+
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,9 +19,9 @@ public class Validacoes {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Object> validaCamposNulosOuVazios(MethodArgumentNotValidException ex) {
+    public ResponseEntity<Object> validaCamposNulosOuVazios(MethodArgumentNotValidException exception) {
         Map<String, String> camposVazios = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(erro -> {
+        exception.getBindingResult().getFieldErrors().forEach(erro -> {
             camposVazios.put(erro.getField(), erro.getDefaultMessage());
         });
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(camposVazios);
@@ -26,8 +29,15 @@ public class Validacoes {
 
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<Object> runtimeException(RuntimeException exception) {
+    public ResponseEntity<String> runtimeException(RuntimeException exception) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro encontrado: " + exception.getLocalizedMessage());
     }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public ResponseEntity<String> solicitacaoNaoPermitida(HttpRequestMethodNotSupportedException exception) {
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body("Esse tipo de solicitação não é permitida: " + exception.getMessage());
+    }
+
 
 }
