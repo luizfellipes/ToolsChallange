83a2fe9f8bf4c82c159410ea51d5cc2f92728307
luizfellipes <luiz.13.silva@gmail.com>
2024-01-27 14:47:28 -0400

    Refatorado os testes da service

---
+++ src/test/java/com/example/toolschallanger/service/TransacaoServiceTest.java
@@ -0,0 +1,124 @@
+package com.example.toolschallanger.service;
+
+import com.example.toolschallanger.models.entities.TransacaoModel;
+import com.example.toolschallanger.models.enuns.Status;
+import com.example.toolschallanger.services.TransacaoService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.example.toolschallanger.mocks.MocksDTO.*;
+import static com.example.toolschallanger.mocks.MocksDTO.requestMockDTO;
+import static com.example.toolschallanger.mocks.MocksModel.requestMockModel;
+import static com.example.toolschallanger.mocks.MocksModel.responseMockModel;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+public class TransacaoServiceTest {
+
+    private TransacaoService transacaoService;
+
+    @BeforeEach
+    public void setUp() {
+        this.transacaoService = mock(TransacaoService.class);
+    }
+
+    //Caso de sucesso
+    @Test
+    public void deveTestarSave() {
+        when(transacaoService.save(any())).thenReturn(requestMockModel());
+
+        Assertions.assertEquals(requestMockDTO(), responseMockDTO());
+    }
+
+    @Test
+    public void deveTestarEstorno() {
+        when(transacaoService.estorno(any(), any())).thenReturn(requestMockModel());
+
+        Assertions.assertEquals(Status.CANCELADO, responseMockModel().getDescricaoModel().getStatus());
+    }
+
+    @Test
+    public void deveTestarFindAll() {
+        when(transacaoService.findAll()).thenReturn(List.of(requestMockModel()));
+
+        Assertions.assertFalse(transacaoService.findAll().isEmpty());
+    }
+
+    @Test
+    public void deveTestarFindById() {
+        when(transacaoService.findById(any())).thenReturn(Optional.of(requestMockModel()));
+
+        Assertions.assertTrue(transacaoService.findById(requestMockModel().getId()).isPresent());
+    }
+
+    @Test
+    public void deveTestarDelete() {
+        when(transacaoService.deleteById(any())).thenReturn(null);
+
+        Assertions.assertNull(transacaoService.deleteById(requestMockModel().getId()));
+    }
+
+    @Test
+    public void deveTestarUpdate() {
+        TransacaoModel transacaoModel = responseMockModel();
+        when(transacaoService.updateById(any(), any())).thenReturn(transacaoModel);
+
+        TransacaoModel transacaoAtualizada = transacaoService.updateById(responseMockModel().getId(), responseMockDTO());
+
+        Assertions.assertEquals(transacaoModel, transacaoAtualizada);
+    }
+
+
+    //Caso de falha
+    @Test
+    public void deveDarErroAoRealizarSave() {
+        when(transacaoService.save(requestMockNullDTO())).thenThrow(new RuntimeException());
+
+        Assertions.assertThrows(RuntimeException.class, () -> transacaoService.save(requestMockNullDTO()));
+    }
+
+    @Test
+    public void deveDarErroAoRealizarEstorno() {
+        when(transacaoService.estorno(any(), any())).thenThrow(new RuntimeException());
+
+        Assertions.assertThrows(RuntimeException.class, () -> transacaoService.estorno(UUID.randomUUID(), requestMockNullDTO()));
+    }
+
+    @Test
+    public void deveDarErroAoRealizarFindAll() {
+        when(transacaoService.findAll()).thenThrow(new RuntimeException());
+
+        Assertions.assertThrows(RuntimeException.class, () -> transacaoService.findAll());
+    }
+
+    @Test
+    public void deveDarErroAoRealizarFindById() {
+        when(transacaoService.findById(any())).thenThrow(new RuntimeException());
+
+        Assertions.assertThrows(RuntimeException.class, () -> transacaoService.findById(UUID.randomUUID()));
+    }
+
+    @Test
+    public void deveDarErroAoRealizarDelete() {
+        when(transacaoService.deleteById(any())).thenThrow(new RuntimeException());
+
+        Assertions.assertThrows(RuntimeException.class, () -> transacaoService.deleteById(UUID.randomUUID()));
+    }
+
+    @Test
+    public void deveDarErroAoRealizarUpdate() {
+        when(transacaoService.updateById(any(), any())).thenThrow(new RuntimeException());
+
+        Assertions.assertThrows(RuntimeException.class, () -> transacaoService.updateById(UUID.randomUUID(), requestMockNullDTO()));
+    }
+
+}
