93185efd120ed45efc4ad268d2d6d5197ac4d97c
luizfellipes <luiz.13.silva@gmail.com>
2024-02-27 10:54:41 -0400

    refatorado, findAll, DeleteById e updateById

--- src/main/java/com/example/toolschallanger/services/TransacaoService.java
+++ src/main/java/com/example/toolschallanger/services/TransacaoService.java
@@ -7,10 +7,14 @@ import com.example.toolschallanger.models.entities.TransacaoModel;
 import com.example.toolschallanger.models.enuns.Status;
 import com.example.toolschallanger.repositories.TransacaoRepository;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 
 import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 @Service
@@ -24,7 +28,6 @@ public class TransacaoService {
 
     public TransacaoModel save(TransacaoRecordDTO transacaoRecordDTO) {
         TransacaoModel transacaoModel = converterDtoEmEntity(transacaoRecordDTO);
-
         return transacaoRepository.save(transacaoModel);
     }
 
@@ -39,31 +42,32 @@ public class TransacaoService {
     }
 
     public List<TransacaoModel> findAll() {
-        return Optional.of(transacaoRepository.findAll())
-                .filter(listaDeTransacoes -> !listaDeTransacoes.isEmpty())
-                .orElseThrow(() -> new RuntimeException("Não foi possível buscar a transação na base, transação solicitada não existente!"));
+        return transacaoRepository.findAll().stream()
+                .filter(Objects::nonNull)
+                .collect(Collectors.collectingAndThen(Collectors.toList(),
+                        lista -> {
+                            if (lista.isEmpty()) {
+                                throw new RuntimeException("Lista de transações vazia");
+                            }
+                            return lista;
+                        }));
     }
 
     public Optional<TransacaoModel> findById(UUID id) {
         return Optional.ofNullable(transacaoRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Não foi encontrado nenhuma transação correspondente ao ID: " + id)));
+                .orElseThrow(() -> new RuntimeException("ID não existente !")));
     }
 
     public Object deleteById(UUID id) {
         findById(id);
         transacaoRepository.deleteById(id);
-        return new HashMap<>() {{
-            put("O seguinte ID", id + " foi deletado !");
-        }};
+        return Collections.singleton("Foi deletado o seguinte ID" + id);
     }
 
     public TransacaoModel updateById(UUID id, TransacaoRecordDTO transacaoRecordDTO) {
         Optional<TransacaoModel> transacaoExistente = findById(id);
         TransacaoModel transacaoParaAtualizar = converterDtoEmEntity(transacaoRecordDTO);
-        transacaoParaAtualizar.setId(id);
-        transacaoParaAtualizar.getDescricaoModel().setNsu(transacaoExistente.get().getDescricaoModel().getNsu());
-        transacaoParaAtualizar.getDescricaoModel().setCodigoAutorizacao(transacaoExistente.get().getDescricaoModel().getCodigoAutorizacao());
-        transacaoParaAtualizar.getDescricaoModel().setStatus(transacaoExistente.get().getDescricaoModel().getStatus());
+        BeanUtils.copyProperties(transacaoExistente, transacaoParaAtualizar, "id");
         return transacaoRepository.save(transacaoParaAtualizar);
     }
 
